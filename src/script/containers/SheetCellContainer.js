5cb28245cea94f99165ae0c827d4bcdc6b57a31a
Tripurari Shankar <tripurari@idecorama.com>
2017-09-05 19:59:11 +0530

    move keydown to util file

--- src/script/containers/SheetCellContainer.js
+++ src/script/containers/SheetCellContainer.js
@@ -3,47 +3,7 @@ import { connect } from 'react-redux';
 
 import SheetCell from '../components/SheetCell';
 import { setActiveCell, setCellValue } from '../actions/CellActions';
-import { lastColumn, lastRow } from '../store/InitialStore';
-import { isCharacterKeyPress } from '../util/util';
-
-const keyDownHandler = (e, dispatch, columnId, rowNo, isEditable) => {
-  switch (e.keyCode) {
-    case 27: // esc key exit edit mode
-      if (isEditable) {
-        dispatch(setActiveCell((columnId + rowNo), false));
-      }
-      break;
-    case 37: // left arrow
-      if (columnId !== 'a') {
-        const newActiveCell = String.fromCharCode(columnId.charCodeAt(0) - 1) + rowNo;
-        dispatch(setActiveCell(newActiveCell, false));
-      }
-      break;
-    case 38: // up arrow
-      if (rowNo !== 0) {
-        const newActiveCell = columnId + (rowNo - 1);
-        dispatch(setActiveCell(newActiveCell, false));
-      }
-      break;
-    case 39: // right arrow
-      if (columnId !== lastColumn) {
-        const newActiveCell = String.fromCharCode(columnId.charCodeAt(0) + 1) + rowNo;
-        dispatch(setActiveCell(newActiveCell, false));
-      }
-      break;
-    case 40: // down arrow
-      if (rowNo !== lastRow) {
-        const newActiveCell = columnId + (rowNo + 1);
-        dispatch(setActiveCell(newActiveCell, false));
-      }
-      break;
-    default:
-      if (isCharacterKeyPress(e) && !isEditable) {
-        dispatch(setActiveCell((columnId + rowNo), true));
-        dispatch(setCellValue(columnId, rowNo, ''));
-      }
-  }
-};
+import { keyDownHandler } from '../util/util';
 
 
 const mapStateToProps = (state, ownProps) => {
--- src/script/util/util.js
+++ src/script/util/util.js
@@ -1,3 +1,6 @@
+import { setActiveCell, setCellValue } from '../actions/CellActions';
+import { lastColumn, lastRow } from '../store/InitialStore';
+
 export function isCharacterKeyPress(evt) {
   if (typeof evt.which === 'undefined') {
     // This is IE, which only fires keypress events for printable keys
@@ -10,3 +13,42 @@ export function isCharacterKeyPress(evt) {
   }
   return false;
 }
+
+export const keyDownHandler = (e, dispatch, columnId, rowNo, isEditable) => {
+  switch (e.keyCode) {
+    case 27: // esc key exit edit mode
+      if (isEditable) {
+        dispatch(setActiveCell((columnId + rowNo), false));
+      }
+      break;
+    case 37: // left arrow
+      if (columnId !== 'a') {
+        const newActiveCell = String.fromCharCode(columnId.charCodeAt(0) - 1) + rowNo;
+        dispatch(setActiveCell(newActiveCell, false));
+      }
+      break;
+    case 38: // up arrow
+      if (rowNo !== 0) {
+        const newActiveCell = columnId + (rowNo - 1);
+        dispatch(setActiveCell(newActiveCell, false));
+      }
+      break;
+    case 39: // right arrow
+      if (columnId !== lastColumn) {
+        const newActiveCell = String.fromCharCode(columnId.charCodeAt(0) + 1) + rowNo;
+        dispatch(setActiveCell(newActiveCell, false));
+      }
+      break;
+    case 40: // down arrow
+      if (rowNo !== lastRow) {
+        const newActiveCell = columnId + (rowNo + 1);
+        dispatch(setActiveCell(newActiveCell, false));
+      }
+      break;
+    default:
+      if (isCharacterKeyPress(e) && !isEditable) {
+        dispatch(setActiveCell((columnId + rowNo), true));
+        dispatch(setCellValue(columnId, rowNo, ''));
+      }
+  }
+};
